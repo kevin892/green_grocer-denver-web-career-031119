1e19a49d645bbd092e4b443fde20e8ff044c50c6
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 00:34:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ def consolidate_cart(cart)
     coupon_hash = {}
     cart.each do |item, info|
       coupons.each do |coupon|
-        if item == coupon[:item] && info[:count] >= coupon[:num]
+        if item == coupon[:item] && info[:count] > coupon[:num]
           info[:count] =  info[:count] - coupon[:num]
           if coupon_hash["#{item} W/COUPON"]
             coupon_hash["#{item} W/COUPON"][:count] += 1
