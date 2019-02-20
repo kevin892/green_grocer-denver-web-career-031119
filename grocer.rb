15ada2eed62ad9148774ea775411642047dd1be0
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 01:19:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,7 @@ def consolidate_cart(cart)
 def apply_clearance(cart)
   cart.each do |item, info|
     if info[:clearance] == true
-      info[:price] *= 5
+      info[:price] *= 4/5
       binding.pry
 
     end
