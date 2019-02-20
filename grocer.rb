aa8563fca1d8b24832a71e2a92345413bb9883f3
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 00:42:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,12 +49,11 @@ def consolidate_cart(cart)
 
   def checkout(cart, coupons)
  cart = consolidate_cart(cart: cart)
-  end 	  cart = apply_coupons(cart: cart, coupons: coupons)
+    cart = apply_coupons(cart: cart, coupons: coupons)
     cart = apply_clearance(cart: cart)
     result = 0
     cart.each do |food, info|
       result += (info[:price] * info[:count]).to_f
     end
-    e d
     result > 100 ? result * 0.9 : result
   end
