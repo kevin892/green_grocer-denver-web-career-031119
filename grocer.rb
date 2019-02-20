c56177494c45961dca02846bf2bb01e359793f1d
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-20 00:22:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,37 @@
-def consolidate_cart(cart)
-  # code here
-end
+require "pry"
 
-def apply_coupons(cart, coupons)
-  # code here
-end
+def consolidate_cart(cart)
+  hash = {}
+  cart.each do |a|
+    a.each do |item, info|
+      hash[item] ||= info
+      hash[item][:count] ||= 0
+      hash[item][:count] += 1
+      end
+    end
+    hash
+  end
+
+  def apply_coupons(cart:[], coupons:[])
+  # code here	  result = {}
+  # code here#
+  cart.each do |food, info|
+    coupons.each do |coupon|
+      if food == coupon[:item] && info[:count] >= coupon[:num]
+        info[:count] =  info[:count] - coupon[:num]
+        if result["#{food} W/COUPON"]
+          result["#{food} W/COUPON"][:count] += 1
+        else
+          result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+        end
+      end
+    end
+    result[food] = info
+  end
+  result
+end	end
+
+
 
 def apply_clearance(cart)
   # code here
